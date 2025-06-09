2cd137f4c4740f44aafd498c6f895f28a062c41a
Md. Soyeb Islam <soyeb10191@gmail.com>
2025-06-09 23:27:32 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <h2 className="bg-green-500 text-3xl">Car Rental</h2>
+      <Button>Button</Button>
+    </>
+  );
+}
+
+export default App;
